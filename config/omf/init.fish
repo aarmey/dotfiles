628606197c31629058fe654eb47084552e05d52a
Aaron Meyer <git@ameyer.me>
2025-04-25 09:50:49 -0700

    Set up mackup files

--- config/omf/init.fish
+++
@@ -1,7 +0,0 @@
-set -gx PATH ~/.bin $PATH
-set -gx PATH ~/Library/TinyTeX/bin/x86_64-darwin $PATH
-
-alias selfcontrol "sudo /Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl (id -u (whoami)) --install"
-alias sharingOn "sudo  /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -configure -allowAccessFor -allUsers -configure -restart -agent -privs -all"
-alias sharingOff "sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off"
-alias brewdepcount "brewdeps.sh | tr ' ' '\n' | grep -v '^\s*\$/' | sort | uniq -c | sort -bnr"
