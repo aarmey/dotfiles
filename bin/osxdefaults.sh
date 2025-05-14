bcb886009bd47f53deaaeab9f3b851c94a0376d8
Aaron Meyer <git@asmlab.org>
2025-05-14 06:23:16 -0700

    Updated tools

--- bin/osxdefaults.sh
+++ bin/osxdefaults.sh
@@ -1,2 +1,9 @@
 # Always open everything in Finder's list view.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Disable the .DS file creation
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Apply the settings
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
