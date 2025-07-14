8e1c9c6195643d7fec695a541f7018e27e1d7f06
Aaron Meyer <git@asmlab.org>
2025-07-14 14:56:03 -0600

    Move to bin folder

--- bin/zotero-webdav.sh
+++ bin/zotero-webdav.sh
@@ -10,10 +10,18 @@ REMOTE_FOLDER="/Archive/Work/Zotero"
 # The local address and port for the WebDAV server
 WEBDAV_ADDR="localhost:8080"
 
+# --- Security Configuration ---
+WEBDAV_USERNAME="webdav"
+WEBDAV_PASSWORD="2mdk^&slwmebdo"
+
 # --- Script ---
 echo "Starting Rclone WebDAV proxy..."
 echo "Remote: ${REMOTE_NAME}:/${REMOTE_FOLDER}"
 echo "Listening on: http://${WEBDAV_ADDR}"
+echo "Username: ${WEBDAV_USERNAME}"
 echo "Press Ctrl+C to stop the server."
 
-rclone serve webdav ${REMOTE_NAME}:${REMOTE_FOLDER} --addr ${WEBDAV_ADDR}
+rclone serve webdav ${REMOTE_NAME}:${REMOTE_FOLDER} \
+    --addr ${WEBDAV_ADDR} \
+    --user ${WEBDAV_USERNAME} \
+    --pass ${WEBDAV_PASSWORD}
