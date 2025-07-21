e78a40e0c458a02f7be22dded7e158ab52ee1c71
Aaron Meyer <git@asmlab.org>
2025-07-21 05:59:32 -0700

    Move to Box

--- bin/zotero-webdav.sh
+++ bin/zotero-webdav.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # --- Configuration ---
-# Name of your Rclone remote for Google Drive
-REMOTE_NAME="gdrive-personal"
+# Name of your Rclone remote for Box
+REMOTE_NAME="box-work"
 
-# The folder on your Google Drive you want to expose
-REMOTE_FOLDER="/Archive/Work/Zotero"
-
-# The local address and port for the WebDAV server
-WEBDAV_ADDR="localhost:8080"
+# The folder on your Box you want to expose
+REMOTE_FOLDER="/Archive/Zotero"
 
 # --- Security Configuration ---
 WEBDAV_USERNAME="webdav"
@@ -17,11 +14,12 @@ WEBDAV_PASSWORD="2mdk^&slwmebdo"
 # --- Script ---
 echo "Starting Rclone WebDAV proxy..."
 echo "Remote: ${REMOTE_NAME}:/${REMOTE_FOLDER}"
-echo "Listening on: http://${WEBDAV_ADDR}"
+echo "Listening on localhost and tailscale at 8080"
 echo "Username: ${WEBDAV_USERNAME}"
 echo "Press Ctrl+C to stop the server."
 
 rclone serve webdav ${REMOTE_NAME}:${REMOTE_FOLDER} \
-    --addr ${WEBDAV_ADDR} \
+    --addr 100.106.19.88:8080 \
+    --addr 127.0.0.1:8080 \
     --user ${WEBDAV_USERNAME} \
     --pass ${WEBDAV_PASSWORD}
